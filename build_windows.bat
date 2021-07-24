04056d0f5de5c9f12f03d2c364eb97ec0b915b8c
hexomancer <87897589+hexomancer@users.noreply.github.com>
2021-07-24 20:18:59 +0430

    Update build_windows.bat

--- build_windows.bat
+++ build_windows.bat
@@ -15,5 +15,5 @@ cp pdf_viewer\prefs.config sioyek-release-windows\prefs.config
 cp pdf_viewer\prefs_user.config sioyek-release-windows\prefs_user.config
 cp -r pdf_viewer\shaders sioyek-release-windows\shaders
 cp tutorial.pdf sioyek-release-windows\tutorial.pdf
-7z -r sioyek-release-windows.zip sioyek-release-windows
+7z a sioyek-release-windows.zip sioyek-release-windows
 
