74a60be6422664c013b858d77a1c781f68b28310
hexomancer <87897589+hexomancer@users.noreply.github.com>
2021-07-25 19:54:03 +0430

    Update build_mupdf.bat

--- build_mupdf.bat
+++ build_mupdf.bat
@@ -2,8 +2,4 @@ cd mupdf\platform\win32\
 msbuild mupdf.sln /property:Configuration=Debug
 msbuild mupdf.sln /property:Configuration=Release
 ls
-ls mupdf
-ls mupdf\platform
-ls mupdf\platform\win32
-ls mupdf\platform\win32\x64
-ls mupdf\platform\win32\x64\Release
+ls x64\Release
