747e1920fd1b57bb876865b547fa463237bf6977
ahrm <a.hr.mostafavi@gmail.com>
2021-07-25 19:14:56 +0430

    fix the folder name

--- build_mupdf.bat
+++ build_mupdf.bat
@@ -1,4 +1,4 @@
 cd mupdf\platform\win32\
 msbuild mupdf.sln /property:Configuration=Debug
 msbuild mupdf.sln /property:Configuration=Release
-ls mupdf\platform\win32\Release
+ls mupdf\platform\win32\x64\Release
