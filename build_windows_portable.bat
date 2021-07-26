bb5f0f4fda094757745a57de7deb477a354f7f2b
hexomancer <87897589+hexomancer@users.noreply.github.com>
2021-07-26 23:58:47 +0430

    Update build_windows_portable.bat

--- build_windows_portable.bat
+++ build_windows_portable.bat
@@ -14,5 +14,5 @@ cp pdf_viewer\prefs_user.config sioyek-release-windows\prefs_user.config
 cp -r pdf_viewer\shaders sioyek-release-windows\shaders
 cp tutorial.pdf sioyek-release-windows\tutorial.pdf
 windeployqt sioyek-release-windows\sioyek.exe
-7z a sioyek-release-windows.zip sioyek-release-windows
+7z a sioyek-release-windows-portable.zip sioyek-release-windows
 
